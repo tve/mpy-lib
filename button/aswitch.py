0492d6da0c849cabdd04fb1c558474c12eb20139
Thorsten von Eicken <tve@voneicken.com>
2020-05-15 22:44:20 -0700

    add module to support push buttons in asyncio

---
+++ button/aswitch.py
@@ -0,0 +1,58 @@
+# Author: Kevin Köck
+# Stripped down version of Peter Hinch's aswitch.py
+# Created on 2019-10-19
+# Copied from https://github.com/kevinkk525/pysmartnode/blob/dev/pysmartnode/utils/aswitch.py by TvE
+
+__updated__ = "2019-10-19"
+__version__ = "0.1"
+
+import uasyncio as asyncio
+
+type_gen = type((lambda: (yield))())  # Generator type
+
+
+# If a callback is passed, run it and return.
+# If a coro is passed initiate it and return.
+# coros are passed by name i.e. not using function call syntax.
+def launch(func, tup_args):
+    res = func(*tup_args)
+    if isinstance(res, type_gen):
+        loop = asyncio.get_event_loop()
+        loop.create_task(res)
+
+
+class Switch:
+    debounce_ms = 50
+
+    def __init__(self, pin):
+        self.pin = pin  # Should be initialised for input with pullup
+        self._open_func = False
+        self._close_func = False
+        self.switchstate = self.pin.value()  # Get initial state
+        loop = asyncio.get_event_loop()
+        loop.create_task(self.switchcheck())  # Thread runs forever
+
+    def open_func(self, func, args=()):
+        self._open_func = func
+        self._open_args = args
+
+    def close_func(self, func, args=()):
+        self._close_func = func
+        self._close_args = args
+
+    # Return current state of switch (0 = pressed)
+    def __call__(self):
+        return self.switchstate
+
+    async def switchcheck(self):
+        while True:
+            state = self.pin.value()
+            if state != self.switchstate:
+                # State has changed: act on it now.
+                self.switchstate = state
+                if state == 0 and self._close_func:
+                    launch(self._close_func, self._close_args)
+                elif state == 1 and self._open_func:
+                    launch(self._open_func, self._open_args)
+            # Ignore further state changes until switch has settled
+            await asyncio.sleep_ms(Switch.debounce_ms)
