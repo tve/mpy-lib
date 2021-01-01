2619ddc93f036004525e73b34b4fc7e668e890a3
Thorsten von Eicken <tve@voneicken.com>
2020-12-31 17:11:46 -0800

    seven-segments: support 5 pixel wide segments, tweak segment alignment

--- seven-segments/seg7.py
+++ seven-segments/seg7.py
@@ -1,7 +1,9 @@
 # Seven segment display of hex digits.
 # From https://stackoverflow.com/a/35566101/3807231
 
-# Order 7 segments clockwise from top left, with crossbar last.
+from micropython import const
+
+# Order 7 segments clockwise from top one, with crossbar last.
 # Coordinates of each segment are (x0, y0, x1, y1)
 # given as offsets from top left measured in segment lengths.
 offsets = b"\x04\x45\x56\x26\x12\x01\x15"  # same as below encoded in bytes w/ 2bits per digit
@@ -14,6 +16,9 @@ offsets = b"\x04\x45\x56\x26\x12\x01\x15"  # same as below encoded in bytes w/ 2
 #    (0, 0, 0, 1),  # upper left
 #    (0, 1, 1, 1),  # middle
 # )
+
+# Flag horizontal segment direction:
+horiz = (1, 0, 0, 1, 0, 0, 1)
 
 # Segments used for each digit; 0, 1 = off, on.
 digits = b"\x7E\x30\x6D\x79\x33\x5B\x5F\x70\x7F\x7B\x77\x1F\x4E\x3D\x4F\x47\x63"
@@ -50,9 +55,9 @@ ord_oh = const(111)  # degree symbol
 def draw_number(fbuf, num_str, x, y, w=24, h=32, color=1, thick=None):
     if not thick:
         thick = (w + 4) >> 3
-    if thick > 3:
-        thick = 3
-    h = h >> 1
+    if thick & 1 == 0:
+        thick += 1
+    h = h // 2
     for n in num_str.encode("utf-8"):
         if n == ord_oh:
             n = ord_g
@@ -78,6 +83,7 @@ def draw_number(fbuf, num_str, x, y, w=24, h=32, color=1, thick=None):
             else:
                 n = n + 10 - ord_a
             d0 = (thick - 1) >> 1  # minus offset due to thickness
+            d1 = d0 + 1
             for seg in range(7):
                 on = (digits[n] >> (6 - seg)) & 1
                 if not on:
@@ -89,16 +95,37 @@ def draw_number(fbuf, num_str, x, y, w=24, h=32, color=1, thick=None):
                 x1 = (oo >> 2) & 3
                 y1 = oo & 3
                 # xa:ya top-left, xb:yb bot right coords for this segment
-                xa = x + x0 * w + 3 * (2 - y0)  # +3*(2-y) creates slant
+                xa = x + x0 * w + 3 * (2 - y0) - (thick//2) # +3*(2-y) creates slant
                 ya = y + y0 * h
                 xb = x + x1 * w + 3 * (2 - y1)
                 yb = y + y1 * h
-                for t in range(thick):
-                    d = -d0 + t
-                    fbuf.line(xa + d, ya + d, xb + d, yb + d, color)
-            for i in range(2):
-                for j in range(3):
-                    (xc, yc) = (x + i * w + 3 * (2 - j), y + j * h)
-                    fbuf.fill_rect(xc - 2, yc - 2, 5, 5, 1 - color)
+                if horiz[seg]:
+                    for t in range(thick):
+                        d = -d0 + t
+                        da = d if d >= 0 else -d
+                        fbuf.line(xa + d1 + da, ya + d, xb - d1 - da, yb + d, color)
+                else:
+                    for t in range(thick):
+                        d = -d0 + t
+                        da = d if d >= 0 else -d
+                        fbuf.line(xa + d, ya + d1 + da, xb + d, yb - d1 - da, color)
 
             x += w + (w >> 1)
+
+
+def width(num_str, w):
+    x = 0
+    for n in num_str.encode("utf-8"):
+        if n == ord_oh:
+            n = ord_g
+        if n < ord_0 or n > ord_9 and n < ord_a or n > ord_g:
+            # it's not a hex digit
+            if n == ord_dot:
+                x += 1 + (w >> 1)
+            elif n == ord_colon:
+                x += 3 + (w >> 1)
+            elif n == ord_space:
+                x += w + (w >> 1)
+        else:
+            x += w + (w >> 1)
+    return x - (w>>1) + 6 + 2 # back out last spacing, add slant, add thickness
